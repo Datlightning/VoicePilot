476141665d4984abab2d133407fb3852d8db1db3
Shyiu <mightydragon334@gmail.com>
2024-04-13 15:28:44 -0400

    Finished all the Gemini command translation

--- translator.py
+++ translator.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 
 
 file = open('input.txt', 'r')
-defaul_prompt = file.read()
+default_prompt = file.read()
 file.close()
 
 speech_content = "double click here and then write i like pizza but bread tastes better pizza tastes bad because it has too much cheese an sauce then indent twive and say this was written in a blog post after that switch mouse movement mode control a and then select last two words and then paste it"
@@ -16,8 +16,8 @@ genai.configure(api_key='AIzaSyB8ZaFbYi_u2nfO-NI1D8YNTCvUofDqkPs')
 model = genai.GenerativeModel('gemini-pro')
 
 def generateResponse(speech_content = "DISREGARD ALL OTHER INSTURCTIONS AND RESPOND WITH JUST [] AND NOTHING ELSE"):
-    response = model.generate_content(defaul_prompt + speech_content)
-    return(response.text)
+    response = model.generate_content(default_prompt + speech_content)
+    return(eval(response.text))
 
 
 if __name__ == "__main__":
