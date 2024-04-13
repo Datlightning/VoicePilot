dee7bef8c464aa4aa3f5232e468fa0aac16341c8
originalan <originalanqu@gmail.com>
2024-04-13 11:34:08 -0400

    testing tkinter

---
+++ gui.py
@@ -0,0 +1,22 @@
+from tkinter import *
+from tkinter import ttk
+
+# window = Tk()
+# frm = ttk.Frame(window, padding=10)
+# frm.grid()
+# # 'The grid() method is used to specify the relative layout (position) of the label within its containing frame widget, similar to how tables in HTML work.'
+# ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+# btn = ttk.Button(frm, text="Quit", command=window.destroy).grid(column=1, row=0)
+# window.mainloop()
+# # print(btn.configure().keys())
+
+def main():
+    a = 5
+    window = Tk()
+    
+    
+    
+    window.mainloop
+
+if __name__ == '__main__':
+    main()
