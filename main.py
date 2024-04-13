af75584e1348f09e368829ef0ca6620df9c3c3b6
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2024-04-13 09:39:20 -0400

    PUSH + INIT

--- main.py
+++ main.py
@@ -1,26 +1,80 @@
+import random
+import time
+
 import speech_recognition as sr
-import pyttsx3
 
+def recognize_speech_from_mic(recognizer, microphone):
+    """Transcribe speech from recorded from `microphone`.
 
+    Returns a dictionary with three keys:
+    "success": a boolean indicating whether or not the API request was
+               successful
+    "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+    "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+    """
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
 
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
 
-def main(): 
-    recognizer = sr.Recognizer() 
-    while True: 
-        try: 
-            with sr.Microphone() as mic: 
-                recognizer.adjust_for_ambient_noise(mic, duration = 0.2)
-                audio = recognizer.listen(mic) 
-                
-                text = recognizer.recognize_google(audio)
-                text = text.lower()
-                print("text: " + text)
-                
-        except sr.UnknownValueError(): 
-            print("oh no") 
-            
-    return 1 
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
 
 
 if __name__ == "__main__":
-    main()
+    # set the list of words, maxnumber of guesses, and prompt limit
+    
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    # get a random word from the list
+    
+    # format the instructions string
+    
+    # show instructions and wait 3 seconds before starting the game
+    while True: 
+        print("guess") 
+        guess = recognize_speech_from_mic(recognizer, microphone)
+        print(guess)
+
+    # if there was an error, stop the game
+    
+    # show the user the transcription
+    
+    # determine if guess is correct and if any attempts remain
+    
+    # determine if the user has won the game
+    # if not, repeat the loop if user has more attempts
+    # if no attempts left, the user loses the game
+    
