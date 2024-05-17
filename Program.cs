69b05126d8dad9de344e2da803dff2aa848fe9b6
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2024-05-16 21:10:24 -0400

    Working Speech Recognition, broken Translation
    The gemini api is not running well, maybe we can develop our own api?

--- Program.cs
+++ Program.cs
@@ -1,31 +1,36 @@
-﻿using Google.Cloud.Speech.V1;
-using System;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using translate; 
 
-namespace SpeechToTextApiDemo
+
+class Program 
 {
-    public class Program
+	
+    async static Task<string> FromMic(SpeechConfig speechConfig)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("INIT INIT");
-            var speech = SpeechClient.Create();
-            var config = new RecognitionConfig
-            {
-                Encoding = RecognitionConfig.Types.AudioEncoding.Flac,
-                SampleRateHertz = 16000,
-                LanguageCode = LanguageCodes.English.UnitedStates
-            };
-            var audio = RecognitionAudio.FromStorageUri("gs://cloud-samples-tests/speech/brooklyn.flac");         
-            
-            var response = speech.Recognize(config, audio);
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
-            foreach (var result in response.Results)
-            {
-                foreach (var alternative in result.Alternatives)
-                {
-                    Console.WriteLine(alternative.Transcript);
-                }
-            }
-        }
+        Console.WriteLine("Speak into your microphone.");
+        var result = await speechRecognizer.RecognizeOnceAsync();
+        return result.Text;
+    }
+	
+	
+	
+	async static Task<string> translate(string input) {
+		var tran = new translator(); 
+		return translator.translate(input); 
+		
+	}
+
+    async static Task Main(string[] args)
+    {
+        var speechConfig = SpeechConfig.FromSubscription("6addf40afc5c4398bb0da7c9b3ab7d25", "eastus");
+        string result = await translate(await FromMic(speechConfig));
+		Console.WriteLine(result); 
     }
 }
--- obj/Debug/net8.0/HackMCSTIX.AssemblyInfo.cs
+++ obj/Debug/net8.0/HackMCSTIX.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackMCSTIX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8096c67851a975304aa18ee07e27861e876bb7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17bd3241182f8943f8c25c3b2795236ed8009402")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackMCSTIX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackMCSTIX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
